b7e68e77973b1c10a8f6137a2cd2def621c75505
roman <roman@ubu00.(none)>
2015-12-22 22:45:56 +0100

    zFrame: better Image-rasterization (Alpha-Channel-Support)

--- cut4/render/c4dummydevice.cc
+++ cut4/render/c4dummydevice.cc
@@ -13,7 +13,7 @@ bool C4DummyDevice::isStereo() const
 }
 
 //-----------------------------------------------------------------------------
-QImage C4DummyDevice::appendFrame(const C4FrameBuffer *left, const C4FrameBuffer *right)
+QImage C4DummyDevice::appendFrame(const C4FrameBuffer *left, const C4FrameBuffer * /*right*/)
 {
     Q_ASSERT(left);
     QImage ret = left->toImage();
